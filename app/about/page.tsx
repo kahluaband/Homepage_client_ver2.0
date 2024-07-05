7e746ee2b80504fe42db69256cb803e6b6b29a67
letthem <suyeon3662@naver.com>
2024-07-05 15:24:56 +0900

    feat: 정기 공연 및 각종 행사 구현

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -41,8 +41,9 @@ const page = () => {
                 </div>
               </div>
             </div>
-            <div className="w-96 h-[368px] flex justify-center rounded-r-3xl rounded-bl-3xl bg-gray-90 text-gray-0">
-              <p className="w-80 pt-[174px] text-[18px] font-medium leading-[150%]">
+            <div className="w-96 h-[368px] flex flex-col items-center rounded-r-3xl rounded-bl-3xl bg-gray-90 text-gray-0">
+              <div className="flex-grow"></div>
+              <p className="w-80 h-[162px] mb-8 text-lg font-medium leading-[150%]">
                 깔루아 모집 후 직속 선배들의 악기 연주를
                 <br />
                 위한 속성 과외가 준비되어 있습니다.
@@ -69,12 +70,56 @@ const page = () => {
           </div>
         </section>
         {/* 정기 공연 및 각종 행사 */}
-        <section className="">
-          <div></div>
-          <div></div>
-          <div>
-            <div></div>
-            <div></div>
+        <section className="mt-[200px] h-[470px] flex">
+          <div className="w-96 rounded-3xl h-full">
+            <div className="flex">
+              <h3 className="w-[282px] h-[102px] rounded-t-3xl bg-danger-40 text-gray-0">
+                <p className="mt-8 ml-8 text-[32px] font-semibold leading-[150%]">
+                  정기 공연
+                  <br />및 각종 행사
+                </p>
+              </h3>
+              <div className="w-[102px] h-[102px] bg-danger-40 relative">
+                <div className="w-[102px] h-[102px] rounded-bl-3xl z-1000 absolute bg-gray-0">
+                  <div className="w-[78px] h-[78px] absolute bg-gray-5 ml-6 mb-6 rounded-3xl flex justify-center items-center">
+                    <Image
+                      src={aboutmusic}
+                      className="w-12 h-12"
+                      alt="About Music"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="w-96 h-[368px] flex flex-col items-center rounded-r-3xl rounded-bl-3xl bg-danger-40 text-gray-0">
+              <div className="flex-grow"></div>
+              <p className="w-84 h-[189px] mb-8 text-lg font-medium leading-[150%]">
+                밴드의 꽃은 바로 공연! 깔루아는 매년 3월과
+                <br />
+                9월 정기 공연을 열고 있습니다.
+                <br />
+                이 외에도 학교 축제, 학과 축제 등 무대만 있다
+                <br />
+                면 달려가 다양한 공연을 하고 있습니다.
+                <br />
+                공연 전 한두 달간 수십번의 합주를 하며 팀워
+                <br />
+                크를 다지고, 공연 직전에는 런스루와 리허설
+                <br />도 합니다.
+              </p>
+            </div>
+          </div>
+
+          <div className="ml-6">
+            <div className="rounded-3xl w-96 h-[223px] overflow-hidden mb-6">
+              <Image src={aboutband} alt="About Band" />
+            </div>
+            <div className="rounded-3xl w-96 h-[223px] overflow-hidden">
+              <Image src={aboutband} alt="About Band" />
+            </div>
+          </div>
+          <div className="ml-6 rounded-3xl w-96 h-full bg-gray-90 overflow-hidden">
+            <Image src={aboutband} alt="About Band" />
           </div>
         </section>
         {/* 창립제와 연말 송년회 */}
