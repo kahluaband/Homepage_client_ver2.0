ec48ac95cfe13ed6892952e478515f0bb9405956
letthem <suyeon3662@naver.com>
2024-07-05 14:42:48 +0900

    feat: 새내기 첫 합주 구현

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,7 @@
+import Image from 'next/image';
+import aboutband from '@/public/image/about-band.png';
+import aboutmusic from '@/public/image/about-music.svg';
+
 const page = () => {
   return (
     <div className="font-pretendard w-full h-[3468px] flex justify-center">
@@ -18,11 +22,50 @@ const page = () => {
         </section>
         {/* 새내기 첫 합주 */}
         <section className="mt-[72px] h-[470px] flex">
-          <div className="w-96 rounded-3xl h-full bg-gray-90"></div>
-          <div className="ml-6 rounded-3xl w-96 h-full bg-gray-90"></div>
-          <div className="ml-6 rounded-3xl w-96 h-full bg-gray-90">
-            <div></div>
-            <div></div>
+          <div className="w-96 rounded-3xl h-full">
+            <div className="flex">
+              <h3 className="w-[282px] h-[102px] rounded-t-3xl bg-gray-90 text-gray-0">
+                <p className="mt-8 ml-8 text-[32px] font-semibold leading-[150%]">
+                  새내기 첫 합주
+                </p>
+              </h3>
+              <div className="w-[102px] h-[102px] bg-gray-90 relative">
+                <div className="w-[102px] h-[102px] rounded-bl-3xl z-1000 absolute bg-gray-0">
+                  <div className="w-[78px] h-[78px] absolute bg-gray-5 ml-6 mb-6 rounded-3xl flex justify-center items-center">
+                    <Image
+                      src={aboutmusic}
+                      className="w-12 h-12"
+                      alt="About Music"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="w-96 h-[368px] flex justify-center rounded-r-3xl rounded-bl-3xl bg-gray-90 text-gray-0">
+              <p className="w-80 pt-[174px] text-[18px] font-medium leading-[150%]">
+                깔루아 모집 후 직속 선배들의 악기 연주를
+                <br />
+                위한 속성 과외가 준비되어 있습니다.
+                <br />
+                그리고 새내기들끼리 대망의 첫 합주를
+                <br />
+                하게되는데요.
+                <br />
+                이 첫 합주를 기점으로 밴드의 매력에
+                <br />푹 빠지게 될거에요!
+              </p>
+            </div>
+          </div>
+          <div className="ml-6 rounded-3xl w-96 h-full bg-gray-90 overflow-hidden">
+            <Image src={aboutband} alt="About Band" />
+          </div>
+          <div className="ml-6">
+            <div className="rounded-3xl w-96 h-[223px] overflow-hidden mb-6">
+              <Image src={aboutband} alt="About Band" />
+            </div>
+            <div className="rounded-3xl w-96 h-[223px] overflow-hidden">
+              <Image src={aboutband} alt="About Band" />
+            </div>
           </div>
         </section>
         {/* 정기 공연 및 각종 행사 */}
