c23035c6e809b1d41ca557e4f02039b5e65cd035
caminobelllo <alstpgml0812@gmail.com>
2024-07-07 00:36:46 +0900

    design: yearcard 컴포넌트 디자인 수정

--- components/performance/YearCard.tsx
+++ components/performance/YearCard.tsx
@@ -1,49 +1,17 @@
 import React from 'react';
 
 const YearCard = () => {
+  const years = ['ALL', '2024', '2023', '2022', '2019', '2018', '2017', '2016'];
+
   return (
-    // 토글 시 전체 연도 표시
-    <div className="absolute top-0 grid grid-cols-4 bg-gray-0 p-3 mt-4 gap-3 rounded-2xl shadow-[0_0_24px_0px_rgba(27,28,35,0.25)]">
-      <div className="w-[60px] inline-flex px-[12px] py-[4px] justify-center items-center gap-[10px] rounded-[32px] ">
-        <text className="text-gray-50 text-center font-pretendard text-[16px] font-normal leading-6">
-          ALL
-        </text>
-      </div>
-      <div className="w-[60px] inline-flex px-[12px] py-[4px] justify-center items-center gap-[10px] rounded-[32px] ">
-        <text className="text-gray-50 text-center font-pretendard text-[16px] font-normal leading-6">
-          0000
-        </text>
-      </div>
-      <div className="w-[60px] inline-flex px-[12px] py-[4px] justify-center items-center gap-[10px] rounded-[32px] ">
-        <text className="text-gray-50 text-center font-pretendard text-[16px] font-normal leading-6">
-          0000
-        </text>
-      </div>
-      <div className="w-[60px] inline-flex px-[12px] py-[4px] justify-center items-center gap-[10px] rounded-[32px] ">
-        <text className="text-gray-50 text-center font-pretendard text-[16px] font-normal leading-6">
-          0000
-        </text>
-      </div>
-      <div className="w-[60px] inline-flex px-[12px] py-[4px] justify-center items-center gap-[10px] rounded-[32px] ">
-        <text className="text-gray-50 text-center font-pretendard text-[16px] font-normal leading-6">
-          0000
-        </text>
-      </div>
-      <div className="w-[60px] inline-flex px-[12px] py-[4px] justify-center items-center gap-[10px] rounded-[32px] ">
-        <text className="text-gray-50 text-center font-pretendard text-[16px] font-normal leading-6">
-          0000
-        </text>
-      </div>
-      <div className="w-[60px] inline-flex px-[12px] py-[4px] justify-center items-center gap-[10px] rounded-[32px] ">
-        <text className="text-gray-50 text-center font-pretendard text-[16px] font-normal leading-6">
-          0000
-        </text>
-      </div>
-      <div className="w-[60px] inline-flex px-[12px] py-[4px] justify-center items-center gap-[10px] rounded-[32px] ">
-        <text className="text-gray-50 text-center font-pretendard text-[16px] font-normal leading-6">
-          0000
-        </text>
-      </div>
+    <div className="absolute top-8 -left-12 grid grid-cols-4 bg-gray-0 p-3 mt-4 gap-3 rounded-2xl shadow-[0_0_24px_0px_rgba(27,28,35,0.25)]">
+      {years.map((year) => (
+        <div className="px-[12px] py-[4px] justify-center items-center gap-[10px] rounded-[32px] ">
+          <p className="w-[60px] text-gray-50 justify-center items-center text-center font-pretendard text-base font-normal">
+            {year}
+          </p>
+        </div>
+      ))}
     </div>
   );
 };
