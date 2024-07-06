5ac8e76a9bfdc3e2199456b58317ebe9b987f253
caminobelllo <alstpgml0812@gmail.com>
2024-07-07 00:43:00 +0900

    remove: yearcard 삭제

--- components/performance/YearSelector.tsx
+++ components/performance/YearSelector.tsx
@@ -3,7 +3,6 @@ import Image from 'next/image';
 import React, { useState } from 'react';
 import chevron_down from '@/public/image/performance/chevron-down.svg';
 import chevron_up from '@/public/image/performance/chevron-up.svg';
-import YearCard from './YearCard';
 
 // const yearData = {
 //   '2024': [
@@ -172,7 +171,23 @@ const YearSelector = () => {
         </div>
       )}
 
-      {isOpen && <YearCard />}
+      {isOpen && (
+        <div className="absolute top-8 -left-12 grid grid-cols-4 bg-gray-0 p-3 mt-4 gap-3 rounded-2xl shadow-[0_0_24px_0px_rgba(27,28,35,0.25)]">
+          {years.map((year) => (
+            <div
+              key={year}
+              className="px-[12px] py-[4px] justify-center items-center gap-[10px] rounded-[32px] "
+            >
+              <p
+                key={year}
+                className="w-[60px] text-gray-50 justify-center items-center text-center font-pretendard text-base font-normal"
+              >
+                {year}
+              </p>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
