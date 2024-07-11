b1ca2eece18b81440f4603ad2cfa8c7cd03be7a5
limgahyun <limgh2002@gmail.com>
2024-07-11 14:55:25 +0900

    Fix: css깨짐 문제 해결

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,8 +2,10 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx},\r\n    ./components/**/*.{js,ts,jsx,tsx,mdx},\r\n    ./app/**/*.{js,ts,jsx,tsx,mdx},\r\n    ./src/**/*.{js,ts,jsx,tsx,mdx},",
-    "./node_modules/@nextui-org/theme/dist/components/(accordion|divider).js"
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     colors: {
