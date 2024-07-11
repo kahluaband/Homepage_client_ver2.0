eb2cc9b0cbad80bc74400215a686ea65305562ad
limgahyun <limgh2002@gmail.com>
2024-07-12 03:56:02 +0900

    Feat: 지원서 작성 전 notice 페이지 구현

--- app/recruit/apply/page.tsx
+++ app/recruit/apply/page.tsx
@@ -1,20 +1,43 @@
+'use client'
+
+import * as React from 'react';
+import Link from "next/link"
+import { Checkbox } from "@/components/ui/checkbox";
 import { Divider } from "@mui/material";
+import { useEffect, useState } from "react";
 
 const page = () => {
+  const [isChecked, setIsChecked] = React.useState(false);
+
+  const handleCheckboxChange = (checked: boolean) => {
+    setIsChecked(checked);
+  };
+
+  useEffect(() => {
+    console.log("현재 체크여부:" + isChecked);
+  }, [isChecked])
+
   return (
     //반응형에 따라 mx 수정 필요
-    <div className="flex flex-col relative top-16 items-center justify-center text-center mx-[200px] h-auto mt-4 ">
+    <div className="flex flex-col relative top-16 items-center justify-center text-center mx-[200px] h-[1800px] mt-4 ">
         <div className="h-[200px] w-full rounded-t-xl bg-gray-90 flex flex-col mt-4 mx-aut">
           <p className="mt-10 text-gray-0 text-center text-2xl font-semibold leading-[48px]">지원 전 필독사항</p>
           <p className="mt-4 text-gray-20 text-center text-lg  font-normal leading-[27px]">안녕하세요! 홍익대학교 컴퓨터공학과 밴드학회 깔루아입니다.<br/>단순 인원 집계용으로 사용되는 가입 지원서이므로 부담 없이 작성해 주시면 됩니다.</p>
         </div>
-        <div className="h-auto w-full rounded-b-xl border border-gray-15 flex flex-col px-12 py-10">
+        <div className="w-full h-auto rounded-b-xl border border-gray-15 flex flex-col px-12 py-10 text-left">
           <Activity/>
           <VocalNotice/>
           <OtherSessionNotice/>
           <Divider/>
           <Warning/>
+          <div className="w-full h-auto px-6 py-5 bg-gray-5 rounded-[12px]">
+            <Checkbox id="notice" checked={isChecked} onChange={handleCheckboxChange} className="">위 필독사항을 모두 숙지했으며, 개인정보 수집에 동의합니다.</Checkbox>
+          </div>
+          <ul className="text-gray-40 text-[18px] ml-6 pt-2 list-disc list-inside">
+            <li>수집된 모든 개인정보는 모집이 마감된 후 파기됩니다.</li>
+          </ul>
         </div>
+        <Link href="/" className={`flex justify-center items-center text-center h-[60px] w-[384px] text-[18px] rounded-[12px] mt-[40px] mb-[288px] ${isChecked ? "bg-primary-50 text-gray-0" : "bg-gray-10 text-gray-40"}`}>다음</Link>
     </div>
   )
 };
