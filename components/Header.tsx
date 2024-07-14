80d43c7e41fe8fe5e065b9ba249cef76e1d92ce8
caminobelllo <alstpgml0812@gmail.com>
2024-07-14 16:14:59 +0900

    design: header 반응형 디자인

--- components/Header.tsx
+++ components/Header.tsx
@@ -24,21 +24,29 @@ const Header = () => {
 
   return (
     // padding 수정 필요
-    <div className="font-pretendard w-full h-[64px] fixed bg-gray-0 flex flex-row justify-between items-center px-40 min-[360px]:px-0">
-      <div className="min-[1920px]:hidden cursor-pointer px-6">
+    <div className="font-pretendard w-full h-[64px] fixed bg-gray-0 flex flex-row justify-center min-[1920px]:justify-between items-center px-0 min-[1920px]:px-40 ">
+      <div className="min-[1920px]:hidden cursor-pointer fixed left-6">
         <Image src={table_menu} alt="moblie_menu_button" width={24} />
       </div>
       <div>
         <Link href="/" key="home">
           {pathname === '/recruit' ? (
-            <Image src={logo_white} alt="logo-white" height={24} />
+            <Image
+              src={logo_white}
+              alt="logo-white"
+              className="h-4 min-[834px]:h-6"
+            />
           ) : (
-            <Image src={logo_black} alt="logo-black" height={24} />
+            <Image
+              src={logo_black}
+              alt="logo-black"
+              className="h-4 min-[834px]:h-6"
+            />
           )}
         </Link>
       </div>
       <div>
-        <ul className="min-[360px]:hidden flex flex-row gap-[64px]">
+        <ul className="hidden min-[1920px]:flex flex-row gap-[64px]">
           {Url.map((url) => (
             <li
               key={url.name}
