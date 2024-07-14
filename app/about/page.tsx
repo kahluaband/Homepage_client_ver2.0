cd30c4fb2f8a1360949db86412269d879d89d2d1
letthem <suyeon3662@naver.com>
2024-07-14 16:19:07 +0900

    Refactor: Banner 컴포넌트 분리

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -15,25 +15,14 @@ import networking3 from '@/public/image/about/networking3.avif';
 import travel1 from '@/public/image/about/travel1.avif';
 import travel2 from '@/public/image/about/travel2.avif';
 import travel3 from '@/public/image/about/travel3.avif';
+import Banner from '@/components/about/Banner';
 
 const page = () => {
   return (
     <div className="font-pretendard w-full h-[3129px] flex justify-center">
       <div className="mx-[360px] w-full">
         {/* About KAHLUA */}
-        <section className="h-[333px] text-center bg-gray-90 mt-20 rounded-3xl">
-          <h1 className="pt-16 font-mustica font-semibold leading-[130%] text-gray-0 text-[64px]">
-            About KAHLUA
-          </h1>
-          <div className="pt-8 text-xl leading-[150%]">
-            <p className="text-gray-0 font-semibold">매주 월요일은 깔요일!</p>
-            <p className="text-gray-40 font-medium">
-              매주 월요일에는 깔루아 정기 회의 및 뒷풀이가 있는 날입니다.
-              <br />
-              정기 모임 외에도 또 어떤 활동들을 할까요?
-            </p>
-          </div>
-        </section>
+        <Banner />
 
         {/* 새내기 첫 합주 */}
         <section className="mt-[72px] h-[470px] flex">
