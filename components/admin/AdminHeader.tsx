33b1642798bd436d6363e4466df59d27ad070adc
caminobelllo <alstpgml0812@gmail.com>
2024-07-17 18:49:25 +0900

    fix: admin header routing 수정

--- components/admin/AdminHeader.tsx
+++ components/admin/AdminHeader.tsx
@@ -2,15 +2,16 @@
 import Link from 'next/link';
 import React, { useEffect, useState } from 'react';
 import logo_black from '@/public/image/KAHLUA-black.svg';
-import logo_white from '@/public/image/KAHLUA.svg';
 import Image from 'next/image';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 const AdminHeader = () => {
+  const router = useRouter();
+
   return (
     // padding 수정 필요
     <div className="font-pretendard w-full h-[64px] fixed bg-gray-0 flex flex-row justify-between items-center px-[360px]">
-      <Link href="/admin" key="admin">
+      <Link href="/admin/applicant" key="admin">
         <div className="inline-flex gap-5 items-center">
           <Image src={logo_black} alt="logo-black" height={24} />
           <p className="font-mustica text-2xl text-gray-60 font-semibold">
@@ -20,9 +21,25 @@ const AdminHeader = () => {
       </Link>
       <div>
         <ul className="flex flex-row gap-[64px]">
-          <li className="text-lg font-medium">지원 관리</li>
-          <li className="text-lg font-medium">예매 관리</li>
-          <li className="text-lg font-medium text-danger-40">로그아웃</li>
+          <li
+            className="text-lg font-medium cursor-pointer"
+            onClick={() => {
+              router.push('/admin/applicant');
+            }}
+          >
+            지원 관리
+          </li>
+          <li
+            className="text-lg font-medium cursor-pointer"
+            onClick={() => {
+              router.push('/admin/ticketing');
+            }}
+          >
+            예매 관리
+          </li>
+          <li className="text-lg font-medium text-danger-40 cursor-pointer">
+            로그아웃
+          </li>
         </ul>
       </div>
     </div>
