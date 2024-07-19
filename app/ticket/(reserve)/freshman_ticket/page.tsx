0cac2c160c1b63d907b7599e8b7f77b8cad57d09
yumzen <yeom.ji.eun35@gmail.com>
2024-07-19 20:24:20 +0900

    Feat: 신입생 예매 페이지 반응형

--- app/ticket/(reserve)/freshman_ticket/page.tsx
+++ app/ticket/(reserve)/freshman_ticket/page.tsx
@@ -59,14 +59,14 @@ const Freshman_ticket: React.FC = () => {
     }, [userInfo]);
 
     return (
-    <div className="h-[1850px] w-[996px] flex flex-col relative mx-auto top-20 ">
-        <div className="h-[200px] w-full rounded-t-xl bg-gray-90 flex flex-col mx-auto">
+    <div className="h-[1850px] ph:w-full pad:w-[786px] dt:w-[996px] flex flex-col relative mx-auto top-20">
+        <div className="h-[200px] w-full pad:rounded-t-xl bg-gray-90 flex flex-col mx-auto">
             <p className="mt-10 text-gray-0 text-center text-2xl font-semibold leading-[48px]">신입생 티켓 예매</p>
             <p className="mt-4 text-gray-20 text-center text-lg  font-normal leading-[27px]">2024년 3월 정기 공연</p>
             <p className="mt-1 text-gray-20 text-center text-lg  font-normal leading-[27px]">2024.03.01  SAT  18:00</p>
         </div>
         <div className="h-[1395px] w-full rounded-b-xl border border-gray-15 flex flex-col mx-auto">
-            <div className="mx-12 flex flex-col">
+            <div className="mx-4 pad:mx-12 flex flex-col">
                 <MemberSelection description="신입생은 최대 1인 1매 구매 가능합니다." min={1} max={1} ticket={"freshman"}  member={member} setMember={setMember}/>
                 <Bar/>
                 <FreshmanInfo userInfo={userInfo} onInfoChange={handleUserInfoChange}/>
