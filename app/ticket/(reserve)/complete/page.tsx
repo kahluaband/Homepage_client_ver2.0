fc492bf21268ad458518b1ab0c8503c877121ce9
yumzen <yeom.ji.eun35@gmail.com>
2024-07-19 21:25:56 +0900

    Feat: 예매 완료 페이지 반응형

--- app/ticket/(reserve)/complete/page.tsx
+++ app/ticket/(reserve)/complete/page.tsx
@@ -26,15 +26,19 @@ const Complete = () => {
     }, []); 
 
     return (
-    <div className={`w-[996px] flex flex-col relative mx-auto top-20 ${dynamicHeightClass}`}>
-        <div className="h-[200px] w-full rounded-t-xl bg-gray-90 flex mx-auto items-center justify-center">
-            <p className="h-12 text-gray-0 text-center text-2xl font-semibold leading-[48px]">예매가 완료되었습니다.</p>
+    <div className={`ph:w-full pad:w-[786px] dt:w-[996px] flex flex-col relative mx-auto top-20 ${dynamicHeightClass}`}>
+        <div className="relative w-full h-[200px] pad:rounded-t-xl overflow-hidden">
+            <div className="absolute inset-0 bg-ticket-complete bg-center bg-cover filter blur-[6px] z-[-1]"></div>
+            <div className="relative flex h-full items-center justify-center pad:bg-gray-90 bg-opacity-60 rounded-t-xl">
+                <p className="h-12 text-gray-0 text-center text-2xl font-semibold leading-[48px]">예매가 완료되었습니다.</p>
+            </div>
         </div>
-        <div className="h-[517px] w-full rounded-b-xl border border-gray-15 flex flex-col mx-auto">
+        <div className="h-[691px] pad:h-[642px] dt:h-[517px] w-full pad:rounded-b-xl pad:border pad:border-gray-15 flex flex-col mx-auto">
             <TicketStatus reservation_id={reservationId as string} />
             <MustRead/>
         </div>
-        <Link href="/ticket/" className="mt-10 w-[384px] h-[59px] flex flex-shrink-0 text-center justify-center items-center mx-auto rounded-xl text-[18px] font-medium text-gray-60 bg-gray-5">예매 페이지로 돌아가기</Link>
+        <Link href="/ticket/" className="mt-14 pad:mt-10 w-[328px] pad:w-[384px] h-[59px] flex flex-shrink-0 text-center justify-center items-center mx-auto rounded-xl text-[18px] font-medium text-gray-60 bg-gray-5">예매 페이지로 돌아가기</Link>
+        <div className="h-[123px] pad:h-[164px]"/>
     </div>
     );
 };
--- components/templates/ticket/TicketStatus.tsx
+++ components/templates/ticket/TicketStatus.tsx
@@ -7,25 +7,33 @@ interface TicketStatusProps {
 
 const TicketStatus: React.FC<TicketStatusProps> = ({ reservation_id }) => {
     return(
-        <div className="flex flex-row mx-12 my-10">
-            <Image src="/image/ticket/Poster.svg" alt="포스터사진" width={198} height={263} className="flex flex-shrink-0 rounded-xl"/>
-            <div className="flex flex-col ml-8 mt-2">
-                <p className="h-[36px] text-[24px] font-semibold leading-6 text-gray-90 ">2024년 3월 정기 공연</p>
-                <p className="h-[30px] text-[20px] font-medium leading-5 text-gray-40">2024.03.01  SAT  18:00</p>
-                <div className="w-[670px] h-[1px] bg-gray-10 flex flex-shrink-0 mt-6"/> 
-                <div className="flex flex-row h-[27px] text-[18px] font-medium leading-7 mt-7">
-                    <p className="text-gray-40 w-[63px]">예매자</p>
-                    <p className="text-gray-80 w-[129px] ml-[39px]">깔루아</p>
-                    <p className="text-gray-40 w-[67px] ml-[104px]">예매번호</p>
-                    <p className="text-gray-80 w-[135px] ml-[39px]">{reservation_id}</p>
+        <div className="flex flex-row mx-4 pad:mx-12 my-10">
+            <Image src="/image/ticket/Poster.svg" alt="포스터사진" width={289} height={357} className="hidden pad:flex flex-shrink-0 rounded-xl dt:w-[198px] dt:h-[263px]"/>
+            <div className="flex flex-col pad:ml-8 mt-2 w-full">
+                <p className="h-[36px] text-[22px] pad:text-[24px] font-semibold leading-6 text-gray-90 ">2024년 3월 정기 공연</p>
+                <p className="h-[30px] text-[18px] pad:text-[20px] font-medium leading-5 text-gray-40">2024.03.01  SAT  18:00</p>
+                <div className="w-full pad:w-[389px] dt:w-[670px] h-[1px] bg-gray-10 flex flex-shrink-0 mt-6"/> 
+                <div className="flex flex-col dt:flex-row text-[16px] pad:text-[18px] font-medium leading-7 mt-4 pad:mt-7">
+                    <div className="flex flex-row h-[27px]">
+                        <p className="text-gray-40 w-[63px]">예매자</p>
+                        <p className="text-gray-80 w-[129px] ml-[39px]">깔루아</p>
+                    </div>
+                    <div className="flex flex-row h-[27px] mt-4 pad:mt-7">
+                        <p className="text-gray-40 w-[67px] dt:ml-[104px]">예매번호</p>
+                        <p className="text-gray-80 w-[135px] ml-[39px]">{reservation_id}</p>
+                    </div>
                 </div>
-                <div className="flex flex-row h-[27px] text-[18px] font-medium leading-7 mt-6">
-                    <p className="text-gray-40 w-[63px]">전화번호</p>
-                    <p className="text-gray-80 w-[129px] ml-[39px]">010-1234-5678</p>
-                    <p className="text-gray-40 w-[67px] ml-[104px]">예매상태</p>
-                    <p className="text-gray-80 w-[135px] ml-[39px]">예매 완료</p>
+                <div className="flex flex-col dt:flex-row text-[16px] pad:text-[18px] font-medium leading-7 mt-4 pad:mt-7">
+                    <div className="flex flex-row h-[27px]">
+                        <p className="text-gray-40 w-[63px]">전화번호</p>
+                        <p className="text-gray-80 w-[129px] ml-[39px]">010-1234-5678</p>
+                    </div>
+                    <div className="flex flex-row h-[27px] mt-4 pad:mt-7">
+                        <p className="text-gray-40 w-[67px] dt:ml-[104px]">예매상태</p>
+                        <p className="text-gray-80 w-[135px] ml-[39px]">예매 완료</p>
+                    </div>
                 </div>
-                <div className="flex flex-row h-[27px] text-[18px] font-medium leading-7 mt-6">
+                <div className="flex flex-row h-[27px] text-[18px] font-medium leading-7 mt-4 pad:mt-7">
                     <p className="text-gray-40 w-[63px]">학번</p>
                     <p className="text-gray-80 w-[129px] ml-[39px]">C123456</p>
                 </div>
