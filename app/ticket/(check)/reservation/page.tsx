3648c51e8f68a50a69cf9f2c2fab1c49c4f3750f
yumzen <yeom.ji.eun35@gmail.com>
2024-07-20 11:25:23 +0900

    Feat: 취소 완료 페이지 반응형

--- app/ticket/(check)/reservation/page.tsx
+++ app/ticket/(check)/reservation/page.tsx
@@ -41,17 +41,17 @@ const ReservationDetails = () => {
         }, []); 
 
     return (
-        <div className={`w-[996px] flex flex-col relative mx-auto top-20 ${dynamicHeightClass}`}>
-            <div className="h-[200px] w-full rounded-t-xl bg-gray-90 flex flex-col mx-auto items-center justify-center gap-4">
+        <div className={`w-full pad:w-[786px] dt:w-[996px] flex flex-col relative mx-auto top-20 ${dynamicHeightClass}`}>
+            <div className="h-[200px] w-full pad:rounded-t-xl bg-gray-90 flex flex-col mx-auto items-center justify-center gap-4">
                 <p className="h-12 text-gray-0 text-center text-2xl font-semibold leading-[48px]">예매 내역 조회</p>
                 <p className="text-gray-20 text-center text-lg  font-normal leading-[27px]">티켓 예매 내역을 확인하고 취소할 수 있습니다.</p>
             </div>
-            <div className="h-[343px] w-full rounded-b-xl border border-gray-15 flex mx-auto">
+            <div className="h-full pad:h-[437px] dt:h-[691px] w-full pad:rounded-b-xl pad:border pad:border-gray-15 flex mx-auto">
                 <TicketStatus reservation_id={reservationId as string} />
             </div>
-            <div className="flex flex-row mt-10 gap-6 text-[18px] font-medium mx-auto">
-                <Link href="/ticket/" className="w-[384px] h-[59px] flex flex-shrink-0 text-center justify-center items-center rounded-xl text-gray-60 bg-gray-5">예매 페이지로 돌아가기</Link>
-                <button onClick={handleOpenModal} className="w-[384px] h-[59px] flex flex-shrink-0 text-center justify-center items-center rounded-xl bg-gray-0 text-danger-40 border border-danger-40">예매 취소하기</button>
+            <div className="flex flex-col pad:flex-row mt-10 gap-[18px] dt:gap-6 text-[18px] font-medium mx-auto">
+                <Link href="/ticket/" className="w-[328px] pad:w-[384px] h-[59px] flex flex-shrink-0 text-center justify-center items-center rounded-xl text-gray-60 bg-gray-5">예매 페이지로 돌아가기</Link>
+                <button onClick={handleOpenModal} className="w-[328px] pad:w-[384px] h-[59px] flex flex-shrink-0 text-center justify-center items-center rounded-xl bg-gray-0 text-danger-40 border border-danger-40">예매 취소하기</button>
             </div>
             <CancelModal
                 isOpen={isModalOpen}
