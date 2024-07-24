89284cb58e2ce5e491109a5aa0537b797535e9fe
서가영 <alstpgml0812@gmail.com>
2024-07-24 23:17:52 +0900

    feat: 기본 login 로직 구현

--- components/admin/LoginBox.tsx
+++ components/admin/LoginBox.tsx
@@ -1,26 +1,36 @@
+'use client';
+
 import Image from 'next/image';
 import React, { useState } from 'react';
 import bg_logo from '@/public/image/admin/bg_KAHLUA.svg';
-import { api } from '@/api';
+import { axiosInstance } from '@/api/auth/axios';
+import { useRouter } from 'next/navigation';
 
 const LoginBox = () => {
-  const [id, setId] = useState('');
+  const router = useRouter();
+
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    if (name === 'id') {
-      setId(value);
-    } else if (name === 'password') {
-      setPassword(value);
-    }
+  const handleEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
   };
 
   const handleLogin = async () => {
-    const id = 'id';
-    const password = 'password';
     try {
-      const response = await api.post('/auth/sign-in', { id, password });
+      const response = await axiosInstance.post('/auth/sign-in', {
+        email,
+        password,
+      });
+      console.log(response.data);
+
+      if (response.data.isSuccess) {
+        router.push('/admin/ticketing');
+      }
     } catch (error) {
       console.error(error);
     }
@@ -45,7 +55,10 @@ const LoginBox = () => {
             <div className="mb-[40px] flex flex-col gap-2">
               <span className="text-xl font-semibold text-gray-90">ID</span>
               <input
-                type="text"
+                type="email"
+                name="email"
+                value={email}
+                onChange={handleEmail}
                 placeholder="ID를 입력해주세요."
                 className="w-[588px] h-12 rounded-xl border-solid border-[1px] border-gray-15 pl-4 placeholder:text-base placeholder:text-gray-20 placeholder:font-normal"
               />
@@ -56,14 +69,20 @@ const LoginBox = () => {
               </span>
               <input
                 type="password"
+                name="password"
+                value={password}
+                onChange={handlePassword}
                 placeholder="비밀번호를 입력해주세요."
                 className="w-[588px] h-12 rounded-xl border-solid border-[1px] border-gray-15 pl-4 placeholder:text-base placeholder:text-gray-20 placeholder:font-normal"
               />
             </div>
           </form>
         </div>
-      </div>{' '}
-      <div className="w-[384px] h-[59px] bg-gray-5 rounded-xl flex justify-center items-center">
+      </div>
+      <div
+        className="w-[384px] h-[59px] bg-gray-5 rounded-xl flex justify-center items-center"
+        onClick={handleLogin}
+      >
         <span className="font-pretendard text-gray-60 text-center text-lg font-medium">
           로그인
         </span>
