b07f3967e9bfa93ff1227ba64690a535f4369ddf
letthem <suyeon3662@naver.com>
2024-07-28 15:26:39 +0900

    Fix: 배경 수정

--- app/dev/page.tsx
+++ app/dev/page.tsx
@@ -1,10 +1,25 @@
+'use client';
 import Image from 'next/image';
 import logo_white from '@/public/image/KAHLUA.svg';
+import DevCard from '@/components/dev/DevCard';
+import { useEffect, useState } from 'react';
 
 const page = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  // redering과정에서 document를 사용할 수 없어서 발생하는 문제 해결
+  useEffect(() => {
+    document.addEventListener('scroll', () => {
+      setIsScrolled(true);
+    });
+  }, []);
+
   return (
-    <div>
-      <div className="flex top-0 justify-center h-auto h-screen w-full dt:h-[480px] pad:h-[753px] h-[322px] bg-performance bg-center text-gray-0 mb-[-160px]">
+    <div className="relative">
+      {isScrolled && (
+        <div className="flex w-full h-[64px] fixed z-10 blur-lg backdrop-blur-sm top-0" />
+      )}
+      <div className="flex top-0 justify-center h-[322px] pad:h-[753px] dt:h-[480px] w-full bg-performance bg-center text-gray-0">
         <div className="flex flex-col dt:mt-[184px] pad:mt-[152px] mt-[120px] items-center text-center w-full pad:w-[786px] dt:w-[1200px]">
           <div className="flex relative pad:w-[376px] pad:h-[64px] w-[235px] h-[40px]">
             <Image src={logo_white} layout="fill" alt="logo" />
@@ -14,8 +29,16 @@ const page = () => {
           </p>
         </div>
       </div>
-      <div className="flex flex-col items-center justify-center bg-gray-90 bg-notice h-auto">
-        <div className="flex flex-col max-pad:px-[16px] w-full pad:w-[786px] dt:w-[1200px]"></div>
+      <div className="bg-gray-90 w-full h-screen"></div>
+      <div className="flex flex-wrap justify-center absolute top-[319px] pad:top-[428px] dt:top-[400px] left-0 w-full h-full">
+        <DevCard />
+        <DevCard />
+        <DevCard />
+        <DevCard />
+        <DevCard />
+        <DevCard />
+        <DevCard />
+        <DevCard />
       </div>
     </div>
   );
---
+++ components/dev/DevCard.tsx
@@ -0,0 +1,5 @@
+const DevCard = () => {
+  return <div className="text-white">카드 굳굳</div>;
+};
+
+export default DevCard;
