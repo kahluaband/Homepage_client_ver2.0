7745901fda99e6d5418571bced34b2f28d9f5289
letthem <suyeon3662@naver.com>
2024-07-28 17:40:38 +0900

    Feat: DevCard 하얀색 추가

--- components/dev/DevCard.tsx
+++ components/dev/DevCard.tsx
@@ -1,31 +1,24 @@
-import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import blurCard from '@/public/image/dev/blurCard.svg';
 import blurCard2 from '@/public/image/dev/blurCard2.svg';
 
-const DevCard: React.FC = () => {
-  const [isSmallScreen, setIsSmallScreen] = useState(false);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsSmallScreen(window.innerWidth <= 360);
-    };
-
-    handleResize();
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-
+const DevCard = () => {
   return (
     <div className="relative w-[328px] h-[172px] pad:w-[384px] pad:h-[210px] dt:w-[384px] dt:h-[210px] mx-auto">
+      <div className="absolute bg-gray-0 rounded-xl flex justify-center items-center pad:w-[92px] pad:h-[92px] w-[68px] h-[68px] mb-3 pad:mb-3"></div>
       <Image
-        src={isSmallScreen ? blurCard2 : blurCard}
+        src={blurCard}
         alt="blurCard"
         layout="fill"
         objectFit="cover"
+        className="pad:block ph:hidden"
+      />
+      <Image
+        src={blurCard2}
+        alt="blurCard2"
+        layout="fill"
+        objectFit="cover"
+        className="pad:hidden ph:block"
       />
     </div>
   );
