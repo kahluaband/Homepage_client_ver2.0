8d3308631e54c249a8047d69a23df11972d91353
letthem <suyeon3662@naver.com>
2024-07-29 11:36:38 +0900

    Feat: ph일 때 DevCard

--- components/dev/DevCard.tsx
+++ components/dev/DevCard.tsx
@@ -13,9 +13,10 @@ interface DevCardProps {
   school: string;
   githubUrl: string;
   githubName: string;
+  index: number;
 }
 
-const DevCard: React.FC<DevCardProps> = ({
+const PadDtCard: React.FC<DevCardProps> = ({
   image,
   name,
   role,
@@ -25,61 +26,9 @@ const DevCard: React.FC<DevCardProps> = ({
   githubName,
 }) => {
   return (
-    <div>
-      {/* pad, dt */}
-      <div className="relative ph:hidden pad:block w-[384px] h-[210px] w-[384px] h-[210px] mx-auto">
-        <div className="relative w-full h-full">
-          <div className="absolute rounded-xl flex justify-center items-center w-[92px] h-[92px] mb-3">
-            <Image
-              src={image}
-              alt={name}
-              layout="fill"
-              objectFit="cover"
-              className="rounded-xl"
-            />
-          </div>
-          <Image
-            src={blurCard}
-            alt="blurCard"
-            layout="fill"
-            objectFit="cover"
-          />
-          <div className="absolute w-full h-full flex">
-            <p className="absolute top-[20px] left-[128px] text-2xl leading-normal text-gray-0 font-semibold">
-              {name}
-            </p>
-            {year && (
-              <div className="absolute top-[22px] left-[203px] w-[55px] h-[32px] rounded-[32px] bg-primary-50 text-gray-0 text-medium text-base text-center py-[4px]">
-                {year}기
-              </div>
-            )}
-            <p className="absolute top-[60px] left-[128px] text-lg leading-normal text-gray-40 font-medium">
-              {role}
-            </p>
-            <div className="absolute top-[126px] left-[24px]">
-              <Image src={schoolIcon} alt="school" />
-            </div>
-            <p className="absolute top-[124px] left-[56px] text-lg leading-normal text-gray-40 font-medium">
-              {school}
-            </p>
-            <div className="absolute top-[165px] left-[24px]">
-              <Image src={githubIcon} alt="github" />
-            </div>
-            <a
-              href={githubUrl}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="absolute top-[163px] left-[56px] text-lg leading-normal text-gray-40 font-medium hover:text-gray-20"
-            >
-              @{githubName}
-            </a>
-          </div>
-        </div>
-      </div>
-
-      {/* ph */}
-      {/* <div className="relative pad:hidden ph:w-[328px] ph:h-[172px] mx-auto">
-        <div className="absolute bg-gray-0 rounded-xl flex justify-center items-center w-[68px] h-[68px] left-[260px]">
+    <div className="relative ph:hidden pad:block w-[384px] h-[210px] mx-auto">
+      <div className="relative w-full h-full">
+        <div className="absolute rounded-xl flex justify-center items-center w-[92px] h-[92px] mb-3">
           <Image
             src={image}
             alt={name}
@@ -88,54 +37,120 @@ const DevCard: React.FC<DevCardProps> = ({
             className="rounded-xl"
           />
         </div>
-        <div className="relative w-full h-full">
-          <Image
-            src={blurCard2}
-            alt="blurCard2"
-            layout="fill"
-            objectFit="cover"
-            className="ph:block pad:hidden"
-          />
-          <Image
-            src={blurCard3}
-            alt="blurCard3"
-            layout="fill"
-            objectFit="cover"
-            className="block"
-          />
-          <div className="absolute w-full h-full flex">
-            <p className="absolute top-[19px] left-[20px] text-xl leading-normal text-gray-0 font-semibold">
-              {name}
-            </p>
-            {year && (
-              <div className="absolute top-[18px] left-[84px] w-[55px] h-[32px] rounded-[32px] bg-primary-50 text-gray-0 text-medium text-base text-center py-[4px]">
-                {year}기
-              </div>
-            )}
-            <p className="absolute top-[56px] left-[20px] text-base pad:text-lg leading-normal text-gray-40 font-medium">
-              {role}
-            </p>
-            <div className="absolute top-[96px] left-[20px]">
-              <Image src={schoolIcon} alt="school" />
+        <Image src={blurCard} alt="blurCard" layout="fill" objectFit="cover" />
+        <div className="absolute w-full h-full flex">
+          <p className="absolute top-[20px] left-[128px] text-2xl leading-normal text-gray-0 font-semibold">
+            {name}
+          </p>
+          {year && (
+            <div className="absolute top-[22px] left-[203px] w-[55px] h-[32px] rounded-[32px] bg-primary-50 text-gray-0 text-medium text-base text-center py-[4px]">
+              {year}기
             </div>
-            <p className="absolute top-[96px] left-[52px] text-base leading-normal text-gray-40 font-medium">
-              {school}
-            </p>
-            <div className="absolute top-[132px] left-[20px]">
-              <Image src={githubIcon} alt="github" />
-            </div>
-            <a
-              href={githubUrl}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="absolute top-[132px] left-[52px] text-base leading-normal text-gray-40 font-medium hover:text-gray-20"
-            >
-              @{githubName}
-            </a>
+          )}
+          <p className="absolute top-[60px] left-[128px] text-lg leading-normal text-gray-40 font-medium">
+            {role}
+          </p>
+          <div className="absolute top-[126px] left-[24px]">
+            <Image src={schoolIcon} alt="school" />
           </div>
+          <p className="absolute top-[124px] left-[56px] text-lg leading-normal text-gray-40 font-medium">
+            {school}
+          </p>
+          <div className="absolute top-[165px] left-[24px]">
+            <Image src={githubIcon} alt="github" />
+          </div>
+          <a
+            href={githubUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="absolute top-[163px] left-[56px] text-lg leading-normal text-gray-40 font-medium hover:text-gray-20"
+          >
+            @{githubName}
+          </a>
         </div>
-      </div> */}
+      </div>
     </div>
+  );
+};
+
+const PhCard: React.FC<DevCardProps> = ({
+  image,
+  name,
+  role,
+  year,
+  school,
+  githubUrl,
+  githubName,
+  index,
+}) => {
+  const isEven = index % 2 === 1;
+  return (
+    <div className="relative pad:hidden ph:block w-[328px] h-[172px] mx-auto">
+      <div
+        className={`absolute bg-gray-0 rounded-xl flex justify-center items-center w-[68px] h-[68px] ${isEven ? 'left-[260px]' : 'left-0'}`}
+      >
+        <Image
+          src={image}
+          alt={name}
+          layout="fill"
+          objectFit="cover"
+          className="rounded-xl"
+        />
+      </div>
+      <div className="relative w-full h-full">
+        <Image
+          src={isEven ? blurCard3 : blurCard2}
+          alt={isEven ? 'blurCard' : 'blurCard'}
+          layout="fill"
+          objectFit="cover"
+        />
+        <div className="absolute w-full h-full flex">
+          <p
+            className={`absolute top-[19px] ${isEven ? 'left-[20px]' : 'left-[104px]'} text-xl leading-normal text-gray-0 font-semibold`}
+          >
+            {name}
+          </p>
+          {year && (
+            <div
+              className={`absolute top-[18px] ${isEven ? 'left-[84px]' : 'left-[168px]'} w-[55px] h-[32px] rounded-[32px] bg-primary-50 text-gray-0 text-medium text-base text-center py-[4px]`}
+            >
+              {year}기
+            </div>
+          )}
+          <p
+            className={`absolute top-[56px] ${isEven ? 'left-[20px]' : 'left-[104px]'} text-base leading-normal text-gray-40 font-medium`}
+          >
+            {role}
+          </p>
+          <div className="absolute top-[96px] left-[20px]">
+            <Image src={schoolIcon} alt="school" />
+          </div>
+          <p className="absolute top-[96px] left-[52px] text-base leading-normal text-gray-40 font-medium">
+            {school}
+          </p>
+          <div className="absolute top-[132px] left-[20px]">
+            <Image src={githubIcon} alt="github" />
+          </div>
+          <a
+            href={githubUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="absolute top-[132px] left-[52px] text-base leading-normal text-gray-40 font-medium hover:text-gray-20"
+          >
+            @{githubName}
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const DevCard: React.FC<DevCardProps> = (props) => {
+  return (
+    <>
+      <PadDtCard {...props} />
+      <PhCard {...props} />
+    </>
   );
 };
 
--- components/dev/DevCardList.tsx
+++ components/dev/DevCardList.tsx
@@ -103,7 +103,7 @@ const DevCardList = () => {
   }, []);
 
   return (
-    <div ref={bgGray90Ref} className="relative w-full bg-gray-90 ">
+    <div ref={bgGray90Ref} className="relative w-full bg-gray-90">
       <div className="inner-div flex justify-center relative top-0 pad:top-[-325px] dt:top-[-80px] w-full ph:pb-20 ph:mb-[-20] pad:pb-[160px] dt:pb-[240px]">
         <div className="grid grid-cols-1 pad:grid-cols-2 dt:grid-cols-3 gap-x-[18px] gap-y-[40px] pad:gap-y-[32px] dt:gap-y-[48px] max-pad:px-[16px] w-full pad:w-[786px] dt:w-[1200px]">
           {devs.map((dev, index) => (
@@ -116,6 +116,7 @@ const DevCardList = () => {
               school={dev.school}
               githubUrl={dev.githubUrl}
               githubName={dev.githubName}
+              index={index}
             />
           ))}
         </div>
