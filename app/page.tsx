b0097b0e42bc668bb9d15f1895efaf03300028ed
limgahyun <limgh2002@gmail.com>
2024-08-10 23:12:42 +0900

    Feat: loading.tsx 생성

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,27 @@
+import Loading from '@/components/loading';
 import About from '@/components/main/About';
 import Performance from '@/components/main/Performance';
 import Recruit from '@/components/main/Recruit';
 import Ticket from '@/components/main/Ticket';
+import { Suspense } from 'react';
 
 export default function Home() {
   return (
-    <div className="font-pretendard flex flex-col relative top-16 w-full h-auto justify-start items-center pad:mt-[32px] -mb-40">
-      <div className="flex flex-col w-full h-auto justify-start items-center max-pad:px-[16px] ">
-        <About />
-        <Performance />
-        <Ticket />
-      </div>
+    <Suspense fallback={<Loading />}>
+      <div className="font-pretendard flex flex-col relative top-16 w-full h-auto justify-start items-center pad:mt-[32px] -mb-40">
+        <div className="flex flex-col w-full h-auto justify-start items-center max-pad:px-[16px] ">
+          <About />
+          <Performance />
+          <Ticket />
+        </div>
 
-      {/* 배경색 변경*/}
-      <p className="flex w-full h-[120px] bg-gradient-to-b from-gray-0 to-gray-90 " />
+        {/* 배경색 변경*/}
+        <p className="flex w-full h-[120px] bg-gradient-to-b from-gray-0 to-gray-90 " />
 
-      <div className="flex flex-col w-full h-auto justify-start items-center max-pad:px-[16px] bg-gray-90 bg-notice">
-        <Recruit />
+        <div className="flex flex-col w-full h-auto justify-start items-center max-pad:px-[16px] bg-gray-90 bg-notice">
+          <Recruit />
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 }
---
+++ components/loading.tsx
@@ -0,0 +1,5 @@
+export default function Loading() {
+  console.log('loading');
+  // You can add any UI inside Loading, including a Skeleton.
+  return <div>loading</div>;
+}
