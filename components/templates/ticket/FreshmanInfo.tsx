aa3ee683a83212d8fa367045a92a5b527f2096f0
서가영 <alstpgml0812@gmail.com>
2024-08-13 19:44:27 +0900

    fix: fix import error

--- components/templates/ticket/FreshmanInfo.tsx
+++ components/templates/ticket/FreshmanInfo.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Input } from '@/components/ui/Input';
+import { Input } from '@/components/ui/input';
 import {
   filterDepartmentValue,
   filterNameValue,
