cb297613a4a395506fab558168083645fdeacc0b
limgahyun <limgh2002@gmail.com>
2024-08-14 13:46:33 +0900

    Feat: 모든 파일 prettier 적용

--- components/templates/ticket/MemberSelection.tsx
+++ components/templates/ticket/MemberSelection.tsx
@@ -1,55 +1,74 @@
-"use client"
+'use client';
 import { useState } from 'react';
 import Image from 'next/image';
 
 type MemberSelectionProps = {
-    description: string;
-    min: number;
-    max: number;
-    ticket: string;
-    member: number; 
-    setMember: React.Dispatch<React.SetStateAction<number>>;
+  description: string;
+  min: number;
+  max: number;
+  ticket: string;
+  member: number;
+  setMember: React.Dispatch<React.SetStateAction<number>>;
 };
 
 const MemberSelection: React.FC<MemberSelectionProps> = ({
-description,
-min,
-max,
-ticket,
-member,
-setMember,
+  description,
+  min,
+  max,
+  ticket,
+  member,
+  setMember,
 }) => {
+  const handleIncrement = () => {
+    setMember((prevMember) => (prevMember < max ? prevMember + 1 : prevMember));
+  };
 
-const handleIncrement = () => {
-    setMember(prevMember => (prevMember < max ? prevMember + 1 : prevMember));
-};
+  const handleDecrement = () => {
+    setMember((prevMember) => (prevMember > min ? prevMember - 1 : prevMember));
+  };
 
-const handleDecrement = () => {
-    setMember(prevMember => (prevMember > min ? prevMember - 1 : prevMember));
-};
+  const finalAmount = 5000 * member;
 
-const finalAmount = 5000 * member;
-
-return (
+  return (
     <div className="flex flex-col mt-10 mb-10 px-4 pad:px-12 h-[83px] w-full">
-        <div className="flex flex-col pad:flex-row h-[55px] pad:h-[30px] gap-1 pad:gap-0">
-            <div className="font-semibold text-lg pad:text-xl leading-[30px] text-gray-90">예매 인원 선택</div>
-            <div className="flex pad:ml-3 font-medium text-[16px] leading-6 text-gray-40 items-center">{description}</div>
+      <div className="flex flex-col pad:flex-row h-[55px] pad:h-[30px] gap-1 pad:gap-0">
+        <div className="font-semibold text-lg pad:text-xl leading-[30px] text-gray-90">
+          예매 인원 선택
         </div>
-        <div className="mt-5 flex flex-row h-[24px] items-center">
-            <div className={`cursor-pointer ${member <= min ? 'opacity-50' : ''}`} onClick={handleDecrement}>
-            <Image src="/image/ticket/subtract.svg" alt="minus" height={24} width={24} />
-            </div>
-            <p className="ml-4">{member}</p>
-            <div className={`cursor-pointer ml-4 ${member >= max ? 'opacity-50' : ''}`}  onClick={handleIncrement}>
-            <Image src="/image/ticket/addplus.svg" alt="plus" height={24} width={24} />
-            </div>
-            <p className="ml-16 text-primary-50 text-lg font-semibold">
-            {ticket === 'freshman' ? '무료' : `${finalAmount.toLocaleString()}원`}
-            </p>
+        <div className="flex pad:ml-3 font-medium text-[16px] leading-6 text-gray-40 items-center">
+          {description}
         </div>
+      </div>
+      <div className="mt-5 flex flex-row h-[24px] items-center">
+        <div
+          className={`cursor-pointer ${member <= min ? 'opacity-50' : ''}`}
+          onClick={handleDecrement}
+        >
+          <Image
+            src="/image/ticket/subtract.svg"
+            alt="minus"
+            height={24}
+            width={24}
+          />
+        </div>
+        <p className="ml-4">{member}</p>
+        <div
+          className={`cursor-pointer ml-4 ${member >= max ? 'opacity-50' : ''}`}
+          onClick={handleIncrement}
+        >
+          <Image
+            src="/image/ticket/addplus.svg"
+            alt="plus"
+            height={24}
+            width={24}
+          />
+        </div>
+        <p className="ml-16 text-primary-50 text-lg font-semibold">
+          {ticket === 'freshman' ? '무료' : `${finalAmount.toLocaleString()}원`}
+        </p>
+      </div>
     </div>
-);
+  );
 };
 
 export default MemberSelection;
--- components/ui/Bar.tsx
+++ components/ui/Bar.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
 interface BarProps {
-    className?: string;
+  className?: string;
 }
 
 const Bar: React.FC<BarProps> = ({ className }) => {
-    return (
-        <div className={`h-[1px] bg-gray-10 flex flex-shrink-0 relative ${className}`} />
-    );
-}
+  return (
+    <div
+      className={`h-[1px] bg-gray-10 flex flex-shrink-0 relative ${className}`}
+    />
+  );
+};
 
 export default Bar;
