0fe1a00af3fb46e9e594d7bcaa02790d1b61e77c
limgahyun <limgh2002@gmail.com>
2024-08-18 01:10:08 +0900

    Feat: recruiting info data 사용

--- components/data/RecruitingInfo.ts
+++ components/data/RecruitingInfo.ts
@@ -1,5 +1,5 @@
 interface recruitingInformation {
-  num: number;
+  num: string;
   recruitingStartDate: Date;
   recruitingFinishDate: Date;
   vocalApplyingDue: Date;
@@ -8,6 +8,13 @@ interface recruitingInformation {
   announcementDate: Date;
   activityPeriod: Date;
 }
+
+// ex. 23
+export const getOnlyNum = (str: string) => {
+  const regex = /[^0-9]/g;
+  const result = str.replace(regex, '');
+  return result;
+};
 
 // ex. 2024
 export const formatYear = (date: Date): string => `${date.getFullYear()}`;
@@ -20,12 +27,20 @@ export const formatMonth = (date: Date): string =>
 export const formatDay = (date: Date): string =>
   `${('0' + date.getDate()).slice(-2)}`;
 
-//'일, 월, 화, 수, 목, 금, 토' 중 해당하는 요일 출력
+// ex. FRI
 export const formatDayofWeek = (date: Date) => {
-  const week = new Array('일', '월', '화', '수', '목', '금', '토');
+  const week = new Array('SUN', 'MON', 'TUE', 'WED', 'THUR', 'FRI', 'SAT');
   const dayOfWeek = date.getDay();
   return week[dayOfWeek];
 };
+
+// ex. 2024.03.11 FRI
+export const formatFullDate = (date: Date): string =>
+  `${formatYear(date)}.${formatMonth(date)}.${formatDay(date)} ${formatDayofWeek(date)}`;
+
+// ex. 03.11 FRI
+export const formatMonthToDate = (date: Date): string =>
+  `${formatMonth(date)}.${formatDay(date)} ${formatDayofWeek(date)}`;
 
 // ex. 16
 export const formatHour = (date: Date): string => `${date.getHours()}`;
@@ -35,11 +50,11 @@ export const formatMinute = (date: Date): string =>
   `${String(date.getMinutes()).padStart(2, '0')}`;
 
 // ex. 16:59
-const formatTime = (date: Date): string =>
+export const formatTime = (date: Date): string =>
   `${date.getHours()}:${String(date.getMinutes()).padStart(2, '0')}`;
 
 export const Recruiting23rd: recruitingInformation = {
-  num: 23,
+  num: '23rd',
   recruitingStartDate: new Date('2024-03-01T00:00:00+09:00'),
   recruitingFinishDate: new Date('2024-03-07T23:59:00+09:00'),
   vocalApplyingDue: new Date('2024-03-08T23:59:00+09:00'),
