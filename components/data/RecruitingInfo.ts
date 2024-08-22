9c4d66bd6f4f43454462a805902dfce43206bccf
yumzen <yeom.ji.eun35@gmail.com>
2024-08-22 10:50:34 +0900

    Feat: Date 동적 생성

--- components/data/RecruitingInfo.ts
+++ components/data/RecruitingInfo.ts
@@ -53,13 +53,37 @@ export const formatMinute = (date: Date): string =>
 export const formatTime = (date: Date): string =>
   `${date.getHours()}:${String(date.getMinutes()).padStart(2, '0')}`;
 
-export const Recruiting23rd: recruitingInformation = {
-  num: '23rd',
-  recruitingStartDate: new Date('2024-03-01T00:00:00+09:00'),
-  recruitingFinishDate: new Date('2024-03-07T23:59:00+09:00'),
-  vocalApplyingDue: new Date('2024-03-08T23:59:00+09:00'),
-  audition: new Date('2024-03-11T16:00:00+09:00'),
-  afterParty: new Date('2024-03-11T19:00:00+09:00'),
-  announcementDate: new Date('2024-03-15T10:00:00+09:00'),
-  activityPeriod: new Date('2026-09-01T18:00:00+09:00'),
+export const recruitingInfo = (
+  num: string,
+  startDate: string,
+  finishDate: string,
+  vocalDue: string,
+  auditionDate: string,
+  afterPartyDate: string,
+  announcementDate: string,
+  activityPeriodDate: string
+): recruitingInformation => {
+  return {
+    num: num,
+    recruitingStartDate: new Date(startDate),
+    recruitingFinishDate: new Date(finishDate),
+    vocalApplyingDue: new Date(vocalDue),
+    audition: new Date(auditionDate),
+    afterParty: new Date(afterPartyDate),
+    announcementDate: new Date(announcementDate),
+    activityPeriod: new Date(activityPeriodDate),
+  };
 };
+
+const dynamicRecruitingInfo = recruitingInfo(
+  '23rd',
+  '2024-03-01T00:00:00+09:00', // 모집 시작일
+  '2024-03-07T23:59:00+09:00', // 모집 마감일
+  '2024-03-08T23:59:00+09:00', // 보컬 지원 마감일
+  '2024-03-11T16:00:00+09:00', // 면접 날짜
+  '2024-03-11T19:00:00+09:00', // 면접 후 모임 날짜
+  '2024-03-15T10:00:00+09:00', // 발표 날짜
+  '2026-09-01T18:00:00+09:00' // 활동 기간
+);
+
+export const Recruiting23rd: recruitingInformation = dynamicRecruitingInfo;
