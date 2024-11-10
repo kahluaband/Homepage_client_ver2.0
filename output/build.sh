b7b3ca5ef253f04a6c0e3bf23ee57f27bafb9648
limgahyun <limgh2002@gmail.com>
2024-11-10 18:25:14 +0900

    Remove: output 폴더 제거

--- output/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cd ../
-mkdir output
-cp -R ./Homepage_client_ver2.0/* ./output
-cp -R ./output ./Homepage_client_ver2.0/
--- output/components/util/delay.ts
+++
@@ -1,8 +0,0 @@
-/**
- * async / await promise delay function
- * @param {number} delay miliseconds 단위의 딜레이 시간
- * @returns {Promise<void>} 비동기 함수. 반환 없음
- */
-async function delay(ms: number) {
-  return new Promise((res) => setTimeout(res, ms));
-}
