90ac746598a019c93da47683bcab2b2e7a8e1490
서가영 <alstpgml0812@gmail.com>
2024-11-14 18:45:19 +0900

    Feat: 리스트 구현

--- app/(kahlua)/mypage/page.tsx
+++ app/(kahlua)/mypage/page.tsx
@@ -1,31 +1,14 @@
 import UserProfile from '@/components/mypage/UserProfile';
-
-interface reservationProps {
-  date: string;
-  time: string;
-  status: string;
-}
-
-const dummyReservation: reservationProps[] = [
-  {
-    date: '2024.10.30 (수)',
-    time: '20:00 - 21:30',
-    status: '개인 : 홍길동',
-  },
-  {
-    date: '2024.10.31 (목)',
-    time: '20:00 - 22:00',
-    status: '팀 : OB1팀',
-  },
-];
+import List from '@/components/mypage/List';
 
 const page = () => {
   return (
-    <div className="font-pretendard w-full flex mt-16">
+    <div className="font-pretendard w-full flex-col mt-16">
       {/* 상단 프로필 섹션*/}
       <UserProfile />
 
       {/* 리스트 섹션 */}
+      <List />
     </div>
   );
 };
---
+++ components/mypage/List.tsx
@@ -0,0 +1,167 @@
+'use client';
+import likeIcon from '@/public/image/notice/EmptyHeart.svg';
+import chatIcon from '@/public/image/notice/chat.svg';
+import Image from 'next/image';
+import { useState } from 'react';
+
+interface reservationProps {
+  date: string;
+  time: string;
+  status: string;
+}
+
+interface myPostProps {
+  title: string;
+  like: number;
+  comment: number;
+  date: string;
+}
+
+interface toggleProps {
+  toggle: string;
+}
+
+const toggleList: Array<toggleProps> = [
+  { toggle: '동방 예약 확인' },
+  { toggle: '내가 쓴 글' },
+];
+
+// dummy data
+const dummyReservation: reservationProps[] = [
+  {
+    date: '2024.10.30 (수)',
+    time: '20:00 - 21:30',
+    status: '개인 : 홍길동',
+  },
+  {
+    date: '2024.10.31 (목)',
+    time: '20:00 - 22:00',
+    status: '팀 : OB1팀',
+  },
+];
+// dummy data
+const dummyMyPost: myPostProps[] = [
+  {
+    title: '제목1',
+    like: 10,
+    comment: 5,
+    date: '2024.10.30',
+  },
+  {
+    title: '제목2',
+    like: 20,
+    comment: 3,
+    date: '2024.10.31',
+  },
+  {
+    title: '제목3',
+    like: 30,
+    comment: 2,
+    date: '2024.11.01',
+  },
+  {
+    title: '제목4',
+    like: 40,
+    comment: 1,
+    date: '2024.11.02',
+  },
+];
+
+export const ReservationList = () => {
+  return (
+    <div>
+      <ul>
+        {dummyReservation.map((reservation) => {
+          return (
+            <li className="flex py-6 items-start gap-4 self-stretch relative border-y-[1px] border-y-solid border-y-gray-10">
+              <p className="text-black text-xl font-semibold">
+                {reservation.date}
+              </p>
+              <p className="text-black text-xl font-semibold">
+                {reservation.time}
+              </p>
+              <div className="flex py-1 px-2 justify-center items-center gap-[10px] rounded-full border-[1px] border-solid border-primary-50">
+                <p className="text-primary-50 text-base font-normal">
+                  {reservation.status}
+                </p>
+              </div>
+              <p className="text-danger-40 text-base font-normal absolute right-0">
+                예약 취소하기
+              </p>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export const MyPostList = () => {
+  return (
+    <div>
+      <ul>
+        {dummyMyPost.map((post) => {
+          return (
+            <li className="flex py-6 items-start gap-4 self-stretch relative border-y-[1px] border-y-solid border-y-gray-10 justify-between">
+              <p>{post.title}</p>
+              <div className="flex gap-10">
+                <div className="flex gap-6">
+                  <div className="flex gap-[10px]">
+                    <Image src={likeIcon} alt="like" width={14} height={14} />
+                    <p>{post.like}</p>
+                  </div>
+                  <div className="flex gap-[10px]">
+                    <Image src={chatIcon} alt="chat" width={18} height={18} />
+                    <p>{post.comment}</p>
+                  </div>
+                </div>
+                <p>{post.date}</p>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+const List = () => {
+  const [toggle, setToggle] = useState('동방 예약 확인');
+
+  const toggleHandler = (toggleItem: string) => {
+    setToggle(toggleItem);
+  };
+
+  return (
+    <div className="flex flex-col mt-6 pad:mt-10 mx-4 pad:mx-6 dt:mx-[150px]">
+      {/* 카테고리 토글 */}
+      <section className="flex mb-6 text-2xl font-semibold">
+        <ul className="flex gap-6">
+          {toggleList.map((category) => {
+            return (
+              <li
+                key={category.toggle}
+                onClick={() => {
+                  toggleHandler(category.toggle);
+                }}
+                className={
+                  toggle === category.toggle ? 'text-black' : 'text-gray-40'
+                }
+              >
+                {category.toggle}
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+
+      {/* 리스트 */}
+      <section className="flex flex-col border-t-[1px] border-t-black border-b-[1px] border-b-black">
+        {toggle === '동방 예약 확인' && <ReservationList />}
+        {toggle === '내가 쓴 글' && <MyPostList />}
+      </section>
+    </div>
+  );
+};
+
+export default List;
