2af4a606d02322a4cf8b494ce8c975893063a14b
서가영 <alstpgml0812@gmail.com>
2024-11-14 17:15:25 +0900

    Feat: mypage 상단 프로필 섹션

--- app/(kahlua)/mypage/page.tsx
+++ app/(kahlua)/mypage/page.tsx
@@ -1,5 +1,33 @@
+import UserProfile from '@/components/mypage/UserProfile';
+
+interface reservationProps {
+  date: string;
+  time: string;
+  status: string;
+}
+
+const dummyReservation: reservationProps[] = [
+  {
+    date: '2024.10.30 (수)',
+    time: '20:00 - 21:30',
+    status: '개인 : 홍길동',
+  },
+  {
+    date: '2024.10.31 (목)',
+    time: '20:00 - 22:00',
+    status: '팀 : OB1팀',
+  },
+];
+
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div className="font-pretendard w-full flex mt-16">
+      {/* 상단 프로필 섹션*/}
+      <UserProfile />
+
+      {/* 리스트 섹션 */}
+    </div>
+  );
 };
 
 export default page;
---
+++ components/mypage/UserProfile.tsx
@@ -0,0 +1,43 @@
+import Image from 'next/image';
+import profileImg from '@/public/image/mypage/profile.png';
+
+interface userProps {
+  name: string;
+  generation: number;
+  session: string;
+}
+
+const dummyUser: userProps = {
+  name: '홍길동',
+  generation: 20,
+  session: '보컬',
+};
+
+const UserProfile = () => {
+  return (
+    <section className="w-full h-[260px] bg-gray-5 py-16 px-6 pad:px-[150px] dt:px-[360px] flex gap-8 justify-between">
+      <div className="flex w-full justify-between items-start self-stretch ">
+        <h1 className="text-gary-90 text-[64px] font-semibold leading-[64px]">
+          My Page
+        </h1>
+        <div className="flex justify-end items-end self-stretch gap-2 font-semibold">
+          <p className="text-gray-90 text-2xl">{dummyUser.name}</p>
+          <div className="flex text-primary-50 text-[22px] gap-1">
+            <p>{dummyUser.generation}기</p>
+            <p>{dummyUser.session}</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <Image
+          src={profileImg}
+          alt="default-profile"
+          width={174}
+          height={174}
+        />
+      </div>
+    </section>
+  );
+};
+
+export default UserProfile;
