45773dc5823848a530a0c2831ea2b96472a3d664
limgahyun <limgh2002@gmail.com>
2024-11-17 17:59:16 +0900

    Feat: admin recruiting info page

--- app/(kahlua)/admin/recruitingInfo/page.tsx
+++ app/(kahlua)/admin/recruitingInfo/page.tsx
@@ -8,10 +8,16 @@ import { defaultData, recruitingInfoList } from './recruitingData';
 import AdminButton from '@/components/ui/admin/Button';
 import isChanged from '@/components/util/isChanged';
 import ButtonModal from '@/components/ui/ButtonModal';
+import Link from 'next/link';
+import WestIcon from '@mui/icons-material/West';
+import Modal from '@/components/ui/Modal';
+import CancelModal from '@/components/admin/CancelModal';
+import EditModal from '@/components/admin/EditModal';
 
 const RecruitingPage = () => {
   const [data, setData] = useState<{ [key: string]: any }>(defaultData); // Explicitly typing data
-  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [isEditModalOpen, setIsEditModalOpen] = useState<boolean>(false);
+  const [isCandelModalOpen, setIsCancelModalOpen] = useState<boolean>(false);
 
   // 수정 취소
   const onCancelEdit = () => {
@@ -24,7 +30,7 @@ const RecruitingPage = () => {
   // 변경 사항 저장
   const onSaveEdit = () => {
     // [todo] api 연결
-    setIsModalOpen(false);
+    setIsEditModalOpen(false);
   };
 
   const onChangeData = (newValue: any, label: string) => {
@@ -33,7 +39,10 @@ const RecruitingPage = () => {
 
   return (
     <div className="font-pretendard mx-auto w-full pad:w-[786px] dt:w-[1200px] h-auto flex flex-col gap-[40px]">
+      {/* Banner */}
       <Banner>모집 정보 수정</Banner>
+
+      {/* List */}
       <div className="flex flex-col w-full max-pad:px-[16px]">
         <InfoList
           data={data}
@@ -41,23 +50,40 @@ const RecruitingPage = () => {
           onChange={onChangeData}
         />
       </div>
+
+      {/* 취소, 저장 Buttons */}
       <div className="flex flex-row gap-[24px] w-full max-pad:px-[16px] justify-end">
         <AdminButton onClick={onCancelEdit}>취소하기</AdminButton>
         <AdminButton
           disabled={!isChanged(defaultData, data)}
-          onClick={() => setIsModalOpen(true)}
+          onClick={() => setIsEditModalOpen(true)}
           className={`${isChanged(defaultData, data) ? 'bg-primary-50' : 'bg-gray-10'}`}
         >
           저장하기
         </AdminButton>
-        <ButtonModal
-          isOpen={isModalOpen}
-          onClose={() => setIsModalOpen(false)}
-          mainContent={<p>모집 정보를 수정하시겠습니까?</p>}
-          buttonContent={<p>수정하기</p>}
-          handleSubmit={() => onSaveEdit()}
+        <EditModal
+          isOpen={isEditModalOpen}
+          setIsOpen={setIsEditModalOpen}
+          handleSubmit={onSaveEdit}
         />
       </div>
+
+      {/* admin 홈으로 Button */}
+      <div className="flex w-auto h-auto max-pad:mx-[16px]">
+        <Link
+          href={'/admin'}
+          key="admin"
+          className="flex flex-row gap-[8px] items-center"
+          onClick={() => setIsCancelModalOpen(true)}
+        >
+          <WestIcon />
+          <span className="text-[16px] font-medium">Admin 홈으로</span>
+        </Link>
+      </div>
+      <CancelModal
+        isOpen={isCandelModalOpen}
+        setIsOpen={setIsCancelModalOpen}
+      />
     </div>
   );
 };
