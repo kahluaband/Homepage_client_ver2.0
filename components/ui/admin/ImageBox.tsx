d827d33adcd5ead07a807c4c1085cf22a181ecea
limgahyun <limgh2002@gmail.com>
2024-11-17 23:00:32 +0900

    Feat: image box 구현 중

--- components/ui/admin/ImageBox.tsx
+++ components/ui/admin/ImageBox.tsx
@@ -4,11 +4,11 @@ import { InputFieldType } from './type';
 interface ImageProps {
   data: any;
   field: InputFieldType;
-  onChange: (newValue: string, title: string) => void;
+  onChange: (newValue: any, label: string) => void;
 }
 
 const ImageBox: React.FC<ImageProps> = ({ data, field, onChange }) => {
-  const { label, title } = field;
+  const { label } = field;
 
   const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
     const files = event.target.files;
@@ -20,17 +20,23 @@ const ImageBox: React.FC<ImageProps> = ({ data, field, onChange }) => {
   };
 
   return (
-    <div
+    <label
       key={label}
-      onClick={() => handleImageUpload}
-      className="flex h-[438px] pad:h-[400px] w-[328px] pad:w-[300px] rounded-[12px] justify-center items-center border-gray-40 border"
+      className="flex h-[438px] pad:h-[400px] w-[328px] pad:w-[300px] rounded-[12px] justify-center items-center border-gray-40 border cursor-pointer"
     >
+      <input
+        type="file"
+        multiple
+        accept="image/*"
+        onChange={handleImageUpload}
+        className="hidden"
+      />
       <img
         src={data}
         alt={`uploaded-${label}`}
         className="w-auto h-full object-cover rounded-[11px] transition duration-200 ease-in-out group-hover:blur-sm"
       />
-    </div>
+    </label>
   );
 };
 
