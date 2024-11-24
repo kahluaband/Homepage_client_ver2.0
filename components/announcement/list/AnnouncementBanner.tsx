95469ed9d6724ff504444cbac5ca4703a3f04ca1
letthem <suyeon3662@naver.com>
2024-11-24 21:23:43 +0900

    [Design] font-pretendard 추가

--- components/announcement/list/AnnouncementBanner.tsx
+++ components/announcement/list/AnnouncementBanner.tsx
@@ -8,7 +8,7 @@ export const AnnouncementBanner = () => {
           <h2 className="text-center text-[36px] pad:text-[64px] font-[600] leading-[130%]">
             ANNOUNCEMENT
           </h2>
-          <p className="text-gray-20 text-[16px] pad:text-[20px] font-[500] text-center mt-8">
+          <p className="font-pretendard text-gray-20 text-[16px] pad:text-[20px] font-[500] text-center mt-8">
             깔루아 멤버들이 이용하는 게시판입니다.
             <br />
             각종 공연 및 행사 관련 정보, 공지사항들을 확인하실 수 있습니다.
