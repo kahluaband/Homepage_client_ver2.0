152fddd48cae849be60f883314dff33f15032b42
letthem <suyeon3662@naver.com>
2024-12-01 18:05:10 +0900

    Fix: CanclePopup ButtonModal 수정 반영

--- components/announcement/posting/CancelPopup.tsx
+++ components/announcement/posting/CancelPopup.tsx
@@ -20,33 +20,19 @@ const CancelPopup: React.FC<CancelPopupProps> = ({
     };
   }, []);
 
-  // 모달 바깥 클릭 시
-  const handleOverlayClick = (
-    event: React.MouseEvent<HTMLDivElement, MouseEvent>
-  ) => {
-    if (event.target === event.currentTarget) {
-      onClose();
-    }
-  };
-
   return (
-    <div
-      onClick={handleOverlayClick}
-      className="fixed z-50 top-0 left-0 right-0 bottom-0 bg-[#0000008a] flex justify-center items-center"
-    >
-      <ButtonModal
-        isOpen={isOpen}
-        onClose={onClose}
-        handleSubmit={onConfirm}
-        mainContent={
-          <>
-            <p>게시글 작성을 취소하시겠습니까?</p>
-            <p>취소 시 작성하던 게시글은 저장되지 않습니다.</p>
-          </>
-        }
-        buttonContent="취소하기"
-      />
-    </div>
+    <ButtonModal
+      isOpen={isOpen}
+      onClose={onClose}
+      handleSubmit={onConfirm}
+      mainContent={
+        <>
+          <p>게시글 작성을 취소하시겠습니까?</p>
+          <p>취소 시 작성하던 게시글은 저장되지 않습니다.</p>
+        </>
+      }
+      buttonContent="취소하기"
+    />
   );
 };
 
