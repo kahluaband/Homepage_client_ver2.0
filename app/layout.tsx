27bff19a61356ebf1caef53d66a64550010a5e10
chaeyoungwon <epopcy@naver.com>
2025-03-03 01:03:08 +0900

    Feat: 링크 썸네일 이미지 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -88,6 +88,7 @@ export default function RootLayout({
               content="안녕하세요 홍익대학교 컴퓨터공학과 밴드부 Kahlua 입니다!"
             />
             <meta property="og:type" content="website" />
+            <meta property="og:image" content="/image/Link_thumbnail.svg" />
             <meta property="og:url" content="https://kahluaband.com" />
           </head>
           <body className={roboto.className}>
