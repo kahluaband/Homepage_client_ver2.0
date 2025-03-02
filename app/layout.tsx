4cea5c983494385a9d4ee5ca9155e8683b041454
원채영 <127955729+chaeyoungwon@users.noreply.github.com>
2025-03-03 08:31:35 +0900

    Fix: 썸네일 등록 반영되도록 버전 정보 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -88,7 +88,7 @@ export default function RootLayout({
               content="안녕하세요 홍익대학교 컴퓨터공학과 밴드부 Kahlua 입니다!"
             />
             <meta property="og:type" content="website" />
-            <meta property="og:image" content="/image/Thumbnail.png" />
+            <meta property="og:image" content="/image/Thumbnail.png?v=1" />
             <meta property="og:url" content="https://kahluaband.com" />
           </head>
           <body className={roboto.className}>
