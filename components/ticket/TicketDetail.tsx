1ffb4a6a0a187b05a193ec71417709105add08d4
dongwook <dongwook@dongwook-MacBookPro.local>
2025-07-06 23:33:40 +0900

    fix: ticket 페이지 스켈레톤 ui 수정

--- components/ticket/TicketDetail.tsx
+++ components/ticket/TicketDetail.tsx
@@ -246,9 +246,75 @@ const TicketDetail = ({ id: prodId }: TicketDetailProps) => {
   }, []);
 
   if (isLoading || !ticketInfo) {
+    // 스켈레톤 UI (반응형)
     return (
-      <div className="flex justify-center items-center w-full h-screen">
-        <p className="text-gray-500 text-lg font-medium" />
+      <div className="flex flex-col pad:flex-row pad:mt-8 pad:h-[328px] w-full pad:w-full dt:h-[376px] dt:justify-center mx-auto animate-pulse">
+        {/* === 포스터(좌측) 영역 === */}
+        <div
+          className={`z-[-1] sticky top-0 w-full h-[300px] mb:w-[300px] pad:w-[246px] pad:h-[328px] dt:w-[282px] dt:h-[376px] mb:rounded-xl mx-auto pad:mx-0 bg-gray-10`}
+        />
+        {/* === 상세 정보(우측) 영역 === */}
+        <div className="z-10 bg-gray-0 flex flex-col w-full h-[355px] mb:w-[350px] pad:w-full dt:w-[338px] px-4 pt-6 pad:pt-0 pad:mt-2 pad:ml-8 mx-auto dt:mr-0 pad:px-0">
+          {/* 상태 뱃지 */}
+          <div className="inline-flex rounded-[32px] gap-2.5 items-center justify-center py-1 px-3 text-[16px] max-w-max bg-gray-10 text-gray-30 w-[80px] h-[32px]" />
+          {/* 제목 & 아이콘 영역 */}
+          <div className="mt-5 pad:mt-4 gap-1 pad:gap-4 flex flex-row items-center">
+            <div className="min-w-[190px] pad:w-[217px] pad:max-w-[217px] h-9 bg-gray-10 rounded-md" />
+            <div className="flex flex-row gap-2 items-center">
+              <div className="h-5 w-5 pad:h-6 pad:w-6 bg-gray-10 rounded-full" />
+              <div className="h-7 w-7 bg-gray-10 rounded-full" />
+            </div>
+          </div>
+          {/* 장소 */}
+          <div className="flex flex-row mt-4 pad:mt-6 text-[16px] pad:text-[18px] leading-9 font-normal gap-6 h-7">
+            <div
+              className="text-gray-40 w-7 pad:w-8 bg-gray-10 rounded"
+              style={{ height: 24 }}
+            />
+            <div className="bg-gray-10 rounded w-32 h-6" />
+          </div>
+          {/* 일시 */}
+          <div className="flex flex-row mt-4 pad:mt-6 text-[16px] pad:text-[18px] leading-9 font-normal gap-6 h-7">
+            <div
+              className="text-gray-40 w-7 pad:w-8 bg-gray-10 rounded"
+              style={{ height: 24 }}
+            />
+            <div className="bg-gray-10 rounded w-32 h-6" />
+          </div>
+          {/* 가격 */}
+          <div className="flex flex-row mt-4 pad:mt-6 text-[16px] pad:text-[18px] leading-9 font-normal">
+            <div className="text-gray-40 w-7 pad:w-8 h-7 bg-gray-10 rounded" />
+            <div className="ml-6 flex flex-col w-full">
+              <div className="flex flex-row items-start h-7 mb-9 pad:mb-12">
+                <div className="bg-gray-10 rounded w-[60px] pad:w-[67px] h-6" />
+                <div className="bg-gray-10 rounded w-[58px] pad:w-[66px] h-6 ml-10" />
+                <div className="bg-gray-10 rounded w-24 h-6 ml-2" />
+              </div>
+            </div>
+          </div>
+          {/* 버튼(예매/영상) */}
+          <div className="max-pad:mx-auto mt-[21px] w-full dt:w-[316px] h-[52px] dt:h-[60px] flex pad:hidden dt:flex flex-shrink-0 text-center items-center justify-center rounded-xl bg-gray-10" />
+        </div>
+        {/* === 모바일/패드 구분선 === */}
+        <div className="z-20 bg-gray-0 h-[40px] w-[100vw] flex pad:hidden items-center" />
+        <div className="flex z-10 flex-shrink-0 pad:hidden w-full mb:w-[328px] pad:w-full h-2 bg-gray-5 mx-auto" />
+        <div className="z-20 bg-gray-0 h-[24px] w-[100vw] flex pad:hidden items-center" />
+        {/* === 데스크탑 지도/주소 영역 === */}
+        <div className="hidden ph:flex z-10 bg-gray-0 pad:hidden dt:flex flex-col w-[100%] px-4 mb:px-0 mb:w-[328px] pad:ml-[164px] h-[282px] pad:mt-[78px] pad:h-full mx-auto">
+          {/* 지도 타이틀 */}
+          <div className="text-[16px] pad:text-[18px] font-medium left-9 text-primary-60 pad:text-primary-50 h-[27px] bg-gray-10 rounded w-24 mb-2" />
+          {/* 주소 & 복사버튼 */}
+          <div className="flex flex-row gap-3 mt-1">
+            <div className="bg-gray-10 rounded w-[194px] pad:w-[294px] h-6" />
+            <div className="flex flex-row cursor-pointer items-center gap-1">
+              <div className="bg-gray-10 rounded w-5 h-5" />
+              <div className="bg-gray-10 rounded w-10 h-5" />
+            </div>
+          </div>
+          {/* 지도 스켈레톤 */}
+          <div className="top-[11px] w-full h-[calc(100vw*192/328)] max-h-[192px] pad:max-h-[225px] mb:w-[328px] pad:w-[376px] mb:h-[192px] pad:h-[225px] rounded-xl flex-shrink-0 z-0 bg-gray-10 mt-2" />
+          <div className="min-h-[164px]" />
+        </div>
       </div>
     );
   }
